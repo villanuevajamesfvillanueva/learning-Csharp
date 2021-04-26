095b6b77c5265ab1bde7440196c1b77dd9322623
James Villanueva <72240605+villanuevajamesfvillanueva@users.noreply.github.com>
2021-04-26 10:13:46 +0800

    Add files via upload
    super basic calc

---
+++ HOA4_Calculator/HOA_Calculator/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace HOA_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.multiply_btn = new System.Windows.Forms.Button();
+            this.divide_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // multiply_btn
+            // 
+            this.multiply_btn.Location = new System.Drawing.Point(25, 132);
+            this.multiply_btn.Name = "multiply_btn";
+            this.multiply_btn.Size = new System.Drawing.Size(234, 36);
+            this.multiply_btn.TabIndex = 0;
+            this.multiply_btn.Text = "MULTIPLY";
+            this.multiply_btn.UseVisualStyleBackColor = true;
+            this.multiply_btn.Click += new System.EventHandler(this.multiply_btn_Click);
+            // 
+            // divide_btn
+            // 
+            this.divide_btn.Location = new System.Drawing.Point(25, 174);
+            this.divide_btn.Name = "divide_btn";
+            this.divide_btn.Size = new System.Drawing.Size(234, 35);
+            this.divide_btn.TabIndex = 1;
+            this.divide_btn.Text = "DIVIDE";
+            this.divide_btn.UseVisualStyleBackColor = true;
+            this.divide_btn.Click += new System.EventHandler(this.divide_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(234, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 235);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.divide_btn);
+            this.Controls.Add(this.multiply_btn);
+            this.Name = "Form1";
+            this.Text = "Basic Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button multiply_btn;
+        private System.Windows.Forms.Button divide_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HOA4_Calculator/HOA_Calculator/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HOA_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void multiply_btn_Click(object sender, EventArgs e)
+        {
+            string textbox1Value = textBox1.Text;
+            string textbox2Value = textBox2.Text;
+
+            // Retrieve as decimal
+            decimal valueDec1 = decimal.Parse(textbox1Value);
+            decimal valueDec2 = decimal.Parse(textbox2Value);
+
+            decimal product = valueDec1 * valueDec2;
+            MessageBox.Show($"The product is {product.ToString()}");
+        }
+
+        private void divide_btn_Click(object sender, EventArgs e)
+        {
+            string textbox1Value = textBox1.Text;
+            string textbox2Value = textBox2.Text;
+
+            // Retrieve as decimal
+            decimal valueDec1 = decimal.Parse(textbox1Value);
+            decimal valueDec2 = decimal.Parse(textbox2Value);
+
+            decimal quotient = valueDec1 / valueDec2;
+            MessageBox.Show($"The quotient is {quotient.ToString()}");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+    }
+}

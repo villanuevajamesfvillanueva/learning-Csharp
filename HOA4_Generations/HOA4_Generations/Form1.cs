3be5bf995f036cbd64ee8b9df03ee29a81cfe18f
James Villanueva <72240605+villanuevajamesfvillanueva@users.noreply.github.com>
2021-04-28 07:30:57 +0800

    Add files via upload

---
+++ HOA4_Generations/HOA4_Generations/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HOA4_Generations
+{
+    public partial class Generations : Form
+    {
+        public Generations()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age = int.Parse(textBox1.Text);
+            string gen;
+            if (age > 55) { gen = "Boomer"; }
+            else if (age > 39 && age <= 55) { gen = "Gen X"; }
+            else if (age > 23 && age <= 39) { gen = "Millenial"; }
+            else { gen = "Gen Z"; }
+            MessageBox.Show($"You are a {gen}!");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

6a3352c696e4ebf3e848b6580ff554f8df57adb7
James Villanueva <72240605+villanuevajamesfvillanueva@users.noreply.github.com>
2021-04-29 01:46:27 +0800

    windows form basic quiz game

---
+++ QuizWindowsForm/QuizWindowsForm/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace QuizWindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelChecker = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.Font = new System.Drawing.Font("LCD Solid", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelQuestion.Location = new System.Drawing.Point(70, 49);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(661, 137);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "Question";
+            this.labelQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(300, 55);
+            this.button2.TabIndex = 2;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(70, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(300, 55);
+            this.button3.TabIndex = 3;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(431, 350);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 55);
+            this.button4.TabIndex = 4;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // labelChecker
+            // 
+            this.labelChecker.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelChecker.Location = new System.Drawing.Point(70, 214);
+            this.labelChecker.Name = "labelChecker";
+            this.labelChecker.Size = new System.Drawing.Size(661, 38);
+            this.labelChecker.TabIndex = 5;
+            this.labelChecker.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelChecker);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QuizWindowsForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelChecker;
+    }
+}
+
---
+++ QuizWindowsForm/QuizWindowsForm/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizWindowsForm
+{
+    public partial class Form1 : Form
+    {
+        //game variables
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            askQuestion(questionNumber);
+            totalQuestions = 10;
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+                labelChecker.Text = "Correct!";
+                labelChecker.ForeColor = System.Drawing.Color.Green;
+            }
+
+            else
+            {
+                labelChecker.Text = "Incorrect";
+                labelChecker.ForeColor = System.Drawing.Color.Red;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                MessageBox.Show("You got " + score + "/" + totalQuestions + Environment.NewLine +
+                    "Click OK to play again");
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+            }
+
+            Check();
+        }
+
+        private async Task Check()
+        {
+            await Task.Delay(1200);
+            questionNumber++;
+            askQuestion(questionNumber);
+        }
+
+        private void askQuestion(int qnum)
+        {
+            switch (qnum)
+            {
+                case 1:
+                    labelQuestion.Text = "How many horns did a triceratops have?";
+                    labelChecker.Text = "";
+                    button1.Text = "2";
+                    button2.Text = "3";
+                    button3.Text = "4";
+                    button4.Text = "5";
+                    correctAnswer = 2;
+                    break;
+
+                case 2:
+                    labelQuestion.Text = "What does the word dinosaur mean?";
+                    labelChecker.Text = "";
+                    button1.Text = "Mighty lizard";
+                    button2.Text = "Scary lizard";
+                    button3.Text = "Terrible lizard";
+                    button4.Text = "Gojira";
+                    correctAnswer = 3;
+                    break;
+
+                case 3:
+                    labelQuestion.Text = "What is the study of fossils called?";
+                    labelChecker.Text = "";
+                    button1.Text = "Paleontology";
+                    button2.Text = "Geology";
+                    button3.Text = "Meteorology";
+                    button4.Text = "Astronomy";
+                    correctAnswer = 1;
+                    break;
+
+                case 4:
+                    labelQuestion.Text = "When did dinosaurs go extinct?";
+                    labelChecker.Text = "";
+                    button1.Text = "Around 65 years ago";
+                    button2.Text = "Around 65 thousand years ago";
+                    button3.Text = "Around 65 million years ago";
+                    button4.Text = "Around 65 billion years ago";
+                    correctAnswer = 3;
+                    break;
+
+                case 5:
+                    labelQuestion.Text = "Why did dinosaurs go extinct?";
+                    labelChecker.Text = "";
+                    button1.Text = "They left the earth aboard an alien spacecraft";
+                    button2.Text = "They were killed by an asteroid";
+                    button3.Text = "They were hunted to extinction";
+                    button4.Text = "Because of overpopulation";
+                    correctAnswer = 2;
+                    break;
+
+                case 6:
+                    labelQuestion.Text = "True or false: birds evolved from dinosaurs";
+                    labelChecker.Text = "";
+                    button1.Text = "True";
+                    button2.Text = "False";
+                    button3.Text = "I don't know";
+                    button4.Text = "I'll pass";
+                    correctAnswer = 1;
+                    break;
+
+                case 7:
+                    labelQuestion.Text = "Which dinosaur had a giraffe-like neck?";
+                    labelChecker.Text = "";
+                    button1.Text = "Stegosaurus";
+                    button2.Text = "Velociraptor";
+                    button3.Text = "T-rex";
+                    button4.Text = "Brachiosaurus";
+                    correctAnswer = 4;
+                    break;
+
+                case 8:
+                    labelQuestion.Text = "Which dinosaur's name means 'fast thief'?";
+                    labelChecker.Text = "";
+                    button1.Text = "Stegosaurus";
+                    button2.Text = "Velociraptor";
+                    button3.Text = "T-rex";
+                    button4.Text = "Brachiosaurus";
+                    correctAnswer = 2;
+                    break;
+
+                case 9:
+                    labelQuestion.Text = "HHow many years did dinosaurs roam the Earth?";
+                    labelChecker.Text = "";
+                    button1.Text = "160";
+                    button2.Text = "160 thousand";
+                    button3.Text = "160 million";
+                    button4.Text = "160 billion";
+                    correctAnswer = 2;
+                    break;
+
+                case 10:
+                    labelQuestion.Text = "During which period did the T-Rex live?";
+                    labelChecker.Text = "";
+                    button1.Text = "Cretaceous";
+                    button2.Text = "Jurassic";
+                    button3.Text = "Triassic";
+                    button4.Text = "1990s";
+                    correctAnswer = 1;
+                    break;
+
+
+            }
+        }
+    }
+}
